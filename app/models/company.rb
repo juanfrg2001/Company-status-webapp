1ed6745d0f36c6d264f434825080bef7260e9981
juanfrg2001 <juanfrg2001@gmail.com>
2022-07-17 14:06:08 -0500

    Adds methosds an seed for comany and reviews

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,3 +1,18 @@
 class Company < ApplicationRecord
-    has_many :company_reviews
+  has_many :company_reviews
+
+  before_create :slugify
+
+
+  def slugify
+    self.slug = name.parameterize
+  end
+
+  def avg_score
+    if (company_reviews.count != 0)
+      company_reviews.average(:score).round(2).to_f
+    else
+      puts("no tiene reviews")
+    end  
+  end 
 end
